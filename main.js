4d2c6e86fd4deffd3b47eda006733aeb1820b276
devmarufurrahaman <mdmarufur5682@gmail.com>
2023-08-31 11:30:44 +0600

    password and date added

--- main.js
+++ main.js
@@ -1,73 +1,134 @@
-const form = document.getElementById("form");
+const form = document.getElementById("inputForm");
 const names = document.getElementById("userName");
 const email = document.getElementById("Email");
 const number = document.getElementById("Number");
 const address = document.getElementById("Address");
+const password = document.getElementById("password");
+const passwordC = document.getElementById("passwordC");
+const date = document.getElementById("date");
+
+// error message variable
+let minChar = "min char ";
+let notBlank = "cannot be blank!";
+let isValid = "is invalid!";
+let passNotMatch = "is not match!";
 
 // more email validate
 function isEmail(emailVal) {
   var atSymbol = emailVal.indexOf("@");
-  if (atSymbol < 1) return false;
-  return true;
+  if (atSymbol < 2) return false;
+  else if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailVal)) {
+    return true;
+  }
+
+  return false;
 }
+
+// validate function
 
 function validate() {
   const nameVal = names.value.trim();
   const emailVal = email.value.trim();
   const numberVal = number.value.trim();
   const addressVal = address.value.trim();
+  const passwordVal = password.value.trim();
+  const passwordCVal = passwordC.value.trim();
+  const dateVal = date.value;
 
   // validate userName
   if (nameVal === "") {
-    setErrorMsg(names, "name cannot be blank");
+    setErrorMsg(names, notBlank);
   } else if (nameVal.length <= 2) {
-    setErrorMsg(names, "name min 3 char");
+    setErrorMsg(names, minChar + 3);
   } else {
     setSuccessMsg(names);
   }
 
   // validate email
   if (emailVal === "") {
-    setErrorMsg(email, "email cannot be blank");
+    setErrorMsg(email, notBlank);
   } else if (!isEmail(emailVal)) {
-    setErrorMsg(email, "not a valid email");
+    setErrorMsg(email, isValid);
   } else {
     setSuccessMsg(email);
   }
 
   // validate number
   if (numberVal === "") {
-    setErrorMsg(number, "number cannot be blank");
-  } else if (numberVal.length <= 10) {
-    setErrorMsg(number, "not a valid number");
-  } else {
+    setErrorMsg(number, notBlank);
+  } else if (numberVal.length === 11) {
     setSuccessMsg(number);
+  } else {
+    setErrorMsg(number, isValid);
   }
 
   // validate address
   if (addressVal === "") {
-    setErrorMsg(address, "address cannot be blank");
+    setErrorMsg(address, notBlank);
   } else if (addressVal.length <= 3) {
-    setErrorMsg(address, "address min 4 char");
+    setErrorMsg(address, minChar + 4);
   } else {
     setSuccessMsg(address);
   }
+
+  // validate date
+  if (dateVal === "") {
+    setErrorMsg(date, notBlank);
+  } else {
+    setSuccessMsg(date);
+  }
+
+  // validate password
+  if (passwordVal === "") {
+    setErrorMsg(password, notBlank);
+  } else if (passwordVal.length <= 5) {
+    setErrorMsg(password, minChar + 6);
+  } else {
+    setSuccessMsg(password);
+  }
+
+  // validate password confirm
+  if (passwordCVal === "") {
+    setErrorMsg(passwordC, notBlank);
+  } else if (passwordVal !== passwordCVal) {
+    setErrorMsg(passwordC, passNotMatch);
+  } else {
+    setSuccessMsg(passwordC);
+  }
 }
+
+// Error Message fn
 
 function setErrorMsg(input, errorMsg) {
   const formControl = input.parentElement;
   const small = formControl.querySelector("small");
+  const inputLabel = formControl.querySelector("label").innerText;
+
   formControl.className = "form-control error";
-  small.innerText = errorMsg;
+  small.innerText = `${inputLabel}  ${errorMsg}`;
 }
+
+// success message fn
 
 function setSuccessMsg(input) {
   const formControl = input.parentElement;
   formControl.className = "form-control success";
 }
 
+// form submition listener
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   validate();
+
+  save();
 });
+
+// form save alert
+
+function save() {
+  let dataStr = $("#inputForm").serialize();
+
+  alert(dataStr);
+}
